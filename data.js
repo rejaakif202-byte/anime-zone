b6ec282e4fa3843d8b794fb8b8490b4ae8f98c2a
rejaakif202-byte <rejaakif202@gmail.com>
2026-04-10 16:19:28 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3,28 +3,4 @@
 // Admin panel se anime add karo yahan data aayega
 // ============================================
 
-const animeData = [{
-    "id": 1,
-    "title": "Demon Slayer",
-    "type": "anime",
-    "genre": [
-      "Action",
-      "Adventure",
-      "Drama",
-      "Fantasy",
-      "Supernatural"
-    ],
-    "rating": 8.3,
-    "year": 2019,
-    "status": "Completed",
-    "studio": "Ufotable",
-    "thumbnail": "https://files.catbox.moe/x6f7k0.jpg",
-    "banner": "https://files.catbox.moe/x6f7k0.jpg",
-    "description": "It is the Taisho Period in Japan. Tanjiro, a kindhearted boy who sells charcoal for a living, finds his family slaughtered by a demon. To make matters worse, his younger sister Nezuko, the sole survivor, has been transformed into a demon herself. Though devastated by this grim reality, Tanjiro resolves to become a “demon slayer” so that he can turn his sister back into a human, and kill the demon that massacred his family.",
-    "trending": true,
-    "latest": false,
-    "top10": false,
-    "top10rank": 0,
-    "seasons": []
-  }
-];
+const animeData = [];
