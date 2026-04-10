6cd76c5cec596fee540500de1d058682413ad899
rejaakif202-byte <rejaakif202@gmail.com>
2026-04-11 05:02:14 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,12 @@
-// =============================================
-// ANIME VERSE — Firebase Config
-// Apna Firebase config yahan paste karo
-// =============================================
-
 const firebaseConfig = {
   apiKey: "AIzaSyAbBza6Zsz8E4fAoo2eilbjhkuEMwIaiKg",
   authDomain: "anime-verse-61b3f.firebaseapp.com",
-  databaseURL: "https://anime-verse-61b3f-default-rtdb.firebaseio.com",
   projectId: "anime-verse-61b3f",
-  storageBucket: "anime-verse-61b3f.firebasestorage.app",
+  storageBucket: "anime-verse-61b3f.appspot.com",
   messagingSenderId: "719532570560",
-  appId: "1:719532570560:web:af5568039e0ccd448dfa25",
-  measurementId: "G-YWYEMHVK4K"
+  appId: "1:719532570560:web:af5568039e0ccd448dfa25"
 };
 
 firebase.initializeApp(firebaseConfig);
-const db      = firebase.firestore();
-const auth    = firebase.auth();
+const db   = firebase.firestore();
+const auth = firebase.auth();
